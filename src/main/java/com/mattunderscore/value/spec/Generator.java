4ab35b13a1f19e0646be047c0b4127015eb252fd
Matt Champion <mattunderscorechampion@gmail.com>
2016-06-09 21:52:23 +0100

    Add draft of code generator.

--- src/main/java/com/mattunderscore/value/spec/Generator.java
+++ src/main/java/com/mattunderscore/value/spec/Generator.java
@@ -26,11 +26,62 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 package com.mattunderscore.value.spec;
 
 import com.mattunderscore.value.spec.model.SpecDesc;
+import com.mattunderscore.value.spec.model.ValueDesc;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.PUBLIC;
 
 /**
+ * Code generator for specification.
  * @author Matt Champion on 05/06/16
  */
 public final class Generator {
-    public void generate(SpecDesc specDesc) {
+    public List<JavaFile> generate(SpecDesc specDesc) {
+        return specDesc
+            .getValues()
+            .stream()
+            .map(this::generateType)
+            .map(typeSpec -> JavaFile.builder(specDesc.getPackageName(), typeSpec).build())
+            .collect(Collectors.toList());
+    }
+
+    private TypeSpec generateType(ValueDesc valueDesc) {
+        final TypeSpec.Builder builder = TypeSpec
+            .classBuilder(valueDesc.getName())
+            .addModifiers(PUBLIC, FINAL)
+            .addJavadoc("Value type $L.\n\nAuto-generated from specification.\n", valueDesc.getName());
+        final MethodSpec.Builder constructor = MethodSpec.constructorBuilder().addModifiers(PUBLIC);
+        valueDesc
+            .getProperties()
+            .stream()
+            .forEach(propertyDesc -> {
+                final ClassName type = ClassName.bestGuess(propertyDesc.getType());
+                final FieldSpec fieldSpec = FieldSpec.builder(type, propertyDesc.getName(), PRIVATE, FINAL).build();
+                final MethodSpec methodSpec = MethodSpec.methodBuilder("get" + propertyDesc.getName())
+                    .addModifiers(PUBLIC)
+                    .addJavadoc("Getter for the property $L\n@returns the value of $L\n", propertyDesc.getName(), propertyDesc.getName())
+                    .returns(type)
+                    .addStatement("return $N", fieldSpec)
+                    .build();
+                final ParameterSpec constructorParameter = ParameterSpec.builder(type, propertyDesc.getName()).build();
+
+                constructor.addParameter(constructorParameter);
+                constructor.addStatement("this.$N = $N", fieldSpec, constructorParameter);
+
+                builder
+                    .addField(fieldSpec)
+                    .addMethod(methodSpec);
+            });
+        return builder.addMethod(constructor.build()).build();
     }
 }
