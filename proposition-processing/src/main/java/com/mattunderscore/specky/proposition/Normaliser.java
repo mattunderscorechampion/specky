b97d8732e1809bb7ef066e6c043b02d3be72dba3
Matt Champion <mattunderscorechampion@gmail.com>
2016-12-05 12:11:12 +0000

    Use jbool_expressions to normalise constraint formula.

--- proposition-processing/src/main/java/com/mattunderscore/specky/proposition/Normaliser.java
+++ proposition-processing/src/main/java/com/mattunderscore/specky/proposition/Normaliser.java
@@ -26,9 +26,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 package com.mattunderscore.specky.proposition;
 
 import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
 
+import java.util.List;
+
+import com.bpodgursky.jbool_expressions.And;
+import com.bpodgursky.jbool_expressions.Expression;
+import com.bpodgursky.jbool_expressions.Or;
+import com.bpodgursky.jbool_expressions.Variable;
+import com.bpodgursky.jbool_expressions.rules.RuleSet;
+import com.mattunderscore.specky.constraint.model.BinaryConstraintOperator;
+import com.mattunderscore.specky.constraint.model.BinaryPropositionExpression;
 import com.mattunderscore.specky.constraint.model.NFConjoinedDisjointPredicates;
 import com.mattunderscore.specky.constraint.model.NFDisjointPredicates;
+import com.mattunderscore.specky.constraint.model.PredicateDesc;
 import com.mattunderscore.specky.constraint.model.Proposition;
 import com.mattunderscore.specky.constraint.model.PropositionalExpression;
 
@@ -41,19 +52,95 @@ public final class Normaliser {
      * Normalise a Propositional expression to conjunctive normal form.
      */
     public NFConjoinedDisjointPredicates normalise(PropositionalExpression expression) {
-        if (expression instanceof Proposition) {
-            return normaliseProposition((Proposition)expression);
-        }
-        throw new UnsupportedOperationException("CNF normalisation not yet implemented");
+        final Expression<PredicateDesc> convertedExpression = convertExpression(expression);
+        final Expression<PredicateDesc> cnf = RuleSet.toCNF(convertedExpression);
+        return convertExpression(cnf);
     }
 
-    private NFConjoinedDisjointPredicates normaliseProposition(Proposition expression) {
+    private Expression<PredicateDesc> convertExpression(PropositionalExpression expression) {
+        if (expression instanceof Proposition) {
+            return convertProposition((Proposition) expression);
+        }
+        else if (expression instanceof BinaryPropositionExpression) {
+            return convertBinaryExpression((BinaryPropositionExpression) expression);
+        }
+        else {
+            throw new IllegalArgumentException("The expression " + expression + " cannot be understood");
+        }
+    }
+
+    private Expression<PredicateDesc> convertProposition(Proposition expression) {
+        return Variable.of(expression.getPredicate());
+    }
+
+    private Expression<PredicateDesc> convertBinaryExpression(BinaryPropositionExpression expression) {
+        final Expression<PredicateDesc> expression0 = convertExpression(expression.getExpression0());
+        final Expression<PredicateDesc> expression1 = convertExpression(expression.getExpression1());
+
+        final BinaryConstraintOperator operation = expression.getOperation();
+        switch (operation) {
+            case CONJUNCTION:
+                return And.of(expression0, expression1);
+            case DISJUNCTION:
+                return Or.of(expression0, expression1);
+            default:
+                throw new IllegalArgumentException("The expression " + expression + " cannot be understood");
+        }
+    }
+
+    private NFConjoinedDisjointPredicates convertExpression(Expression<PredicateDesc> expression) {
+        final String exprType = expression.getExprType();
+
+        switch (exprType) {
+            case And.EXPR_TYPE:
+                return convertAndExpression((And<PredicateDesc>)expression);
+            case Or.EXPR_TYPE:
+                return convertOrExpression((Or<PredicateDesc>)expression);
+            case Variable.EXPR_TYPE:
+                return convertVariableExpression((Variable<PredicateDesc>)expression);
+            default:
+                throw new IllegalArgumentException("The expression " + expression + " cannot be understood");
+        }
+    }
+
+    private NFConjoinedDisjointPredicates convertVariableExpression(Variable<PredicateDesc> expression) {
         return NFConjoinedDisjointPredicates
             .builder()
             .predicates(singletonList(NFDisjointPredicates
                 .builder()
-                .predicates(singletonList(expression.getPredicate()))
+                .predicates(singletonList(expression.getValue()))
                 .build()))
+            .build();
+    }
+
+    private NFConjoinedDisjointPredicates convertOrExpression(Or<PredicateDesc> expression) {
+        final List<PredicateDesc> predicateDescs = expression
+            .getChildren()
+            .stream()
+            .map(expr -> (Variable<PredicateDesc>) expr)
+            .map(Variable::getValue)
+            .collect(toList());
+
+        return NFConjoinedDisjointPredicates
+            .builder()
+            .predicates(singletonList(NFDisjointPredicates
+                .builder()
+                .predicates(predicateDescs)
+                .build()))
+            .build();
+    }
+
+    private NFConjoinedDisjointPredicates convertAndExpression(And<PredicateDesc> expression) {
+        final List<NFDisjointPredicates> disjointPredicates = expression
+            .getChildren()
+            .stream()
+            .map(this::convertExpression)
+            .flatMap((predicates) -> predicates.getPredicates().stream())
+            .collect(toList());
+
+        return NFConjoinedDisjointPredicates
+            .builder()
+            .predicates(disjointPredicates)
             .build();
     }
 }
--- proposition-processing/src/test/java/com/mattunderscore/specky/proposition/NormaliserTest.java
+++ proposition-processing/src/test/java/com/mattunderscore/specky/proposition/NormaliserTest.java
@@ -1,10 +1,13 @@
 package com.mattunderscore.specky.proposition;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.mattunderscore.specky.constraint.model.BinaryConstraintOperator;
+import com.mattunderscore.specky.constraint.model.BinaryPropositionExpression;
 import com.mattunderscore.specky.constraint.model.ConstraintOperator;
 import com.mattunderscore.specky.constraint.model.NFConjoinedDisjointPredicates;
 import com.mattunderscore.specky.constraint.model.NFDisjointPredicates;
@@ -41,5 +44,149 @@ public final class NormaliserTest {
                 .predicates(singletonList(predicate))
                 .build()))
             .build(), cnf);
+    }
+
+    @Test
+    public void normaliseConjunction() {
+        final Normaliser normaliser = new Normaliser();
+
+        final PredicateDesc predicate0 = PredicateDesc
+            .builder()
+            .subject("a")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("b")
+            .build();
+
+        final PredicateDesc predicate1 = PredicateDesc
+            .builder()
+            .subject("c")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("d")
+            .build();
+
+        final NFConjoinedDisjointPredicates cnf = normaliser
+            .normalise(
+                BinaryPropositionExpression
+                    .builder()
+                    .operation(BinaryConstraintOperator.CONJUNCTION)
+                    .expression0(Proposition.builder().predicate(predicate0).build())
+                    .expression1(Proposition.builder().predicate(predicate1).build())
+                    .build());
+
+        assertEquals(NFConjoinedDisjointPredicates
+            .builder()
+            .predicates(asList(
+                NFDisjointPredicates
+                    .builder()
+                    .predicates(singletonList(predicate0))
+                    .build(),
+                NFDisjointPredicates
+                    .builder()
+                    .predicates(singletonList(predicate1))
+                    .build()))
+            .build(), cnf);
+    }
+
+    @Test
+    public void normaliseDisjunction() {
+        final Normaliser normaliser = new Normaliser();
+
+        final PredicateDesc predicate0 = PredicateDesc
+            .builder()
+            .subject("a")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("b")
+            .build();
+
+        final PredicateDesc predicate1 = PredicateDesc
+            .builder()
+            .subject("c")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("d")
+            .build();
+
+        final NFConjoinedDisjointPredicates cnf = normaliser
+            .normalise(
+                BinaryPropositionExpression
+                    .builder()
+                    .operation(BinaryConstraintOperator.DISJUNCTION)
+                    .expression0(Proposition.builder().predicate(predicate0).build())
+                    .expression1(Proposition.builder().predicate(predicate1).build())
+                    .build());
+
+        assertEquals(NFConjoinedDisjointPredicates
+            .builder()
+            .predicates(singletonList(NFDisjointPredicates
+                .builder()
+                .predicates(asList(predicate0, predicate1))
+                .build()))
+            .build(), cnf);
+    }
+
+    @Test
+    public void normaliseComplex() {
+        final Normaliser normaliser = new Normaliser();
+
+        final PredicateDesc predicate0 = PredicateDesc
+            .builder()
+            .subject("a")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("b")
+            .build();
+
+        final PredicateDesc predicate1 = PredicateDesc
+            .builder()
+            .subject("c")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("d")
+            .build();
+
+        final PredicateDesc predicate2 = PredicateDesc
+            .builder()
+            .subject("e")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("f")
+            .build();
+
+        final PredicateDesc predicate3 = PredicateDesc
+            .builder()
+            .subject("g")
+            .operator(ConstraintOperator.EQUAL_TO)
+            .literal("h")
+            .build();
+
+        final NFConjoinedDisjointPredicates cnf = normaliser
+            .normalise(
+                BinaryPropositionExpression
+                    .builder()
+                    .operation(BinaryConstraintOperator.DISJUNCTION)
+                    .expression0(BinaryPropositionExpression
+                        .builder()
+                        .operation(BinaryConstraintOperator.CONJUNCTION)
+                        .expression0(Proposition.builder().predicate(predicate0).build())
+                        .expression1(Proposition.builder().predicate(predicate1).build())
+                        .build())
+                    .expression1(BinaryPropositionExpression
+                        .builder()
+                        .operation(BinaryConstraintOperator.DISJUNCTION)
+                        .expression0(Proposition.builder().predicate(predicate2).build())
+                        .expression1(Proposition.builder().predicate(predicate3).build())
+                        .build())
+                    .build());
+
+        assertEquals(
+            NFConjoinedDisjointPredicates
+                .builder()
+                .predicates(asList(
+                    NFDisjointPredicates
+                        .builder()
+                        .predicates(asList(predicate0, predicate2, predicate3))
+                        .build(),
+                    NFDisjointPredicates
+                        .builder()
+                        .predicates(asList(predicate1, predicate2, predicate3))
+                        .build()))
+                .build(),
+            cnf);
     }
 }
