3aa9b802e760ed128c1fc52c89ced8106269b673
Matt Champion <mattunderscorechampion@gmail.com>
2016-12-24 15:32:32 +0000

    Register licences using a AST listener.

--- ast-transformer/src/main/java/com/mattunderscore/specky/SpeckyFileScopeListener.java
+++ ast-transformer/src/main/java/com/mattunderscore/specky/SpeckyFileScopeListener.java
@@ -1,5 +1,8 @@
 package com.mattunderscore.specky;
 
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import com.mattunderscore.specky.licence.resolver.LicenceResolver;
 import com.mattunderscore.specky.parser.Specky;
 import com.mattunderscore.specky.parser.SpeckyBaseListener;
 import com.mattunderscore.specky.type.resolver.SpecTypeResolver;
@@ -14,13 +17,19 @@ import com.squareup.javapoet.CodeBlock;
 public final class SpeckyFileScopeListener extends SpeckyBaseListener {
     private final SpecTypeResolver typeResolver;
     private final MutableValueResolver valueResolver;
+    private final LicenceResolver licenceResolver;
 
     /**
      * Constructor.
      */
-    public SpeckyFileScopeListener(SpecTypeResolver typeResolver, MutableValueResolver valueResolver) {
+    public SpeckyFileScopeListener(
+        SpecTypeResolver typeResolver,
+        MutableValueResolver valueResolver,
+        LicenceResolver licenceResolver) {
+
         this.typeResolver = typeResolver;
         this.valueResolver = valueResolver;
+        this.licenceResolver = licenceResolver;
     }
 
     @Override
@@ -35,5 +44,41 @@ public final class SpeckyFileScopeListener extends SpeckyBaseListener {
         if (ctx.default_value() != null) {
             valueResolver.register(importTypeName, CodeBlock.of(ctx.default_value().ANYTHING().getText()));
         }
+    }
+
+    @Override
+    public void exitLicenceDeclaration(Specky.LicenceDeclarationContext ctx) {
+        final String licenceText = toValue(ctx.string_value());
+
+        if (ctx.Identifier() != null) {
+            // Register a named licence
+            final String licenceIdentifier = ctx.Identifier().getText();
+            licenceResolver.register(licenceIdentifier, licenceText);
+        }
+        else {
+            // Register the default licence
+            licenceResolver.register(licenceText);
+        }
+    }
+
+    private String toValue(Specky.String_valueContext stringValue) {
+        if (stringValue == null) {
+            return null;
+        }
+
+        final TerminalNode multiline = stringValue.MULTILINE_STRING_LITERAL();
+        final String literal;
+        final int trimLength;
+
+        if (multiline != null) {
+            literal = multiline.getText();
+            trimLength = 3;
+        }
+        else {
+            literal = stringValue.StringLiteral().getText();
+            trimLength = 1;
+        }
+
+        return literal.substring(trimLength, literal.length() - trimLength);
     }
 }
--- ast-transformer/src/test/java/com/mattunderscore/specky/SpeckyFileScopeListenerTest.java
+++ ast-transformer/src/test/java/com/mattunderscore/specky/SpeckyFileScopeListenerTest.java
@@ -1,21 +1,25 @@
 package com.mattunderscore.specky;
 
-import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonToken;
 import org.antlr.v4.runtime.UnbufferedTokenStream;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
-import com.mattunderscore.specky.dsl.model.DSLImportDesc;
+import com.mattunderscore.specky.dsl.model.DSLLicence;
+import com.mattunderscore.specky.licence.resolver.LicenceResolver;
 import com.mattunderscore.specky.parser.Specky;
 import com.mattunderscore.specky.parser.SpeckyLexer;
 import com.mattunderscore.specky.type.resolver.SpecTypeResolver;
@@ -28,6 +32,19 @@ import com.squareup.javapoet.CodeBlock;
  * @author Matt Champion on 23/12/16
  */
 public final class SpeckyFileScopeListenerTest {
+    @Mock
+    private SemanticErrorListener errorListener;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @After
+    public void postConditions() {
+        verifyNoMoreInteractions(errorListener);
+    }
+
     @Test
     public void test() throws IOException {
         final CharStream stream = new ANTLRInputStream(SpeckyFileScopeListenerTest
@@ -39,19 +56,29 @@ public final class SpeckyFileScopeListenerTest {
 
         final SpecTypeResolver typeResolver = new SpecTypeResolver();
         final MutableValueResolver valueResolver = new MutableValueResolver();
-        final SpeckyFileScopeListener listener = new SpeckyFileScopeListener(typeResolver, valueResolver);
+        final LicenceResolver licenceResolver = new LicenceResolver(errorListener);
+        final SpeckyFileScopeListener listener = new SpeckyFileScopeListener(typeResolver, valueResolver, licenceResolver);
         parser.addParseListener(listener);
 
         parser.spec();
 
+        // Verify types
         final Optional<String> i = typeResolver.resolve("Import");
         assertEquals(i.get(), "com.example.Import");
         final Optional<String> v = typeResolver.resolve("Value");
         assertEquals(v.get(), "com.example.Value");
 
+        // Verify default licences
         final Optional<CodeBlock> vdv = valueResolver.resolve(null, "com.example.Value");
         assertEquals(vdv.get(), CodeBlock.of("\"x\""));
-
         assertFalse(valueResolver.resolve(null, "com.example.Import").isPresent());
+
+        // Verify licences
+        final Optional<String> defaultLicence = licenceResolver.resolve(null);
+        assertEquals(defaultLicence.get(), "default licence");
+        final Optional<String> namedLicence = licenceResolver.resolve(DSLLicence.builder().identifier("n").build());
+        assertEquals(namedLicence.get(), "named licence");
+        assertFalse(licenceResolver.resolve(DSLLicence.builder().identifier("u").build()).isPresent());
+        verify(errorListener).onSemanticError("An unknown name u was used to reference a licence");
     }
 }
