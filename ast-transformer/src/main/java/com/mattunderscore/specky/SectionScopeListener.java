3f94978fbcda78cb4a8a7cdbde06d8a8231cbdf0
Matt Champion <mattunderscorechampion@gmail.com>
2016-12-25 12:34:24 +0000

    Add missing licence file headers.

--- ast-transformer/src/main/java/com/mattunderscore/specky/SectionScopeListener.java
+++ ast-transformer/src/main/java/com/mattunderscore/specky/SectionScopeListener.java
@@ -1,5 +1,4 @@
-/* Copyright © 2016 Matthew Champion
-All rights reserved.
+/* Copyright © 2016 Matthew Champion All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
