98f2972042047170e045afd67715d7f0093a9658
Matt Champion <mattunderscorechampion@gmail.com>
2017-01-05 22:34:55 +0000

    Add listener for creating model of abstract types.

---
+++ ast-transformer/src/test/java/com/mattunderscore/specky/AbstractTypeListenerTest.java
@@ -0,0 +1,84 @@
+/* Copyright © 2017 Matthew Champion All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.specky;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.UnbufferedTokenStream;
+import org.junit.Test;
+
+import com.mattunderscore.specky.dsl.model.DSLAbstractTypeDesc;
+import com.mattunderscore.specky.dsl.model.DSLPropertyDesc;
+import com.mattunderscore.specky.parser.Specky;
+import com.mattunderscore.specky.parser.SpeckyLexer;
+
+/**
+ * Unit tests for {@link AbstractTypeListener}.
+ *
+ * @author Matt Champion 05/01/2017
+ */
+public final class AbstractTypeListenerTest {
+
+    @Test
+    public void test() throws IOException {
+        final CharStream stream = new ANTLRInputStream(SectionImportTypeListenerTest
+            .class
+            .getClassLoader()
+            .getResourceAsStream("SectionTest.spec"));
+        final SpeckyLexer lexer = new SpeckyLexer(stream);
+        final Specky parser = new Specky(new UnbufferedTokenStream<CommonToken>(lexer));
+
+        final AbstractTypeListener listener = new AbstractTypeListener();
+        parser.addParseListener(listener);
+
+        parser.spec();
+
+        final List<DSLAbstractTypeDesc> abstractTypeDescs = listener.getAbstractTypeDescs();
+
+        assertEquals(1, abstractTypeDescs.size());
+        assertEquals(
+            DSLAbstractTypeDesc
+                .builder()
+                .name("TestType")
+                .description("Abstract type $L.\n\nAuto-generated from specification.")
+                .properties(asList(
+                    DSLPropertyDesc
+                        .builder()
+                        .name("num")
+                        .type("Integer")
+                        .build()))
+                .build(),
+            abstractTypeDescs.get(0));
+    }
+}
