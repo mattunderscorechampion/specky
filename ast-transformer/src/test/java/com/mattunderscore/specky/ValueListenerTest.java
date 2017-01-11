3109babac9ab7aba422ff24f645ff09a805ae73e
Matt Champion <mattunderscorechampion@gmail.com>
2017-01-11 22:40:26 +0000

    Add value type implementation listener.

---
+++ ast-transformer/src/test/java/com/mattunderscore/specky/ValueListenerTest.java
@@ -0,0 +1,146 @@
+package com.mattunderscore.specky;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toMap;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.UnbufferedTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.mattunderscore.specky.constraint.model.NFConjoinedDisjointPredicates;
+import com.mattunderscore.specky.model.AbstractTypeDesc;
+import com.mattunderscore.specky.model.ConstructionMethod;
+import com.mattunderscore.specky.model.PropertyDesc;
+import com.mattunderscore.specky.model.ValueDesc;
+import com.mattunderscore.specky.model.generator.scope.SectionScopeResolver;
+import com.mattunderscore.specky.parser.Specky;
+import com.mattunderscore.specky.parser.SpeckyLexer;
+import com.mattunderscore.specky.type.resolver.SpecTypeResolver;
+import com.squareup.javapoet.CodeBlock;
+
+/**
+ * Unit tests for {@link AbstractTypeListener}.
+ *
+ * @author Matt Champion 09/01/2017
+ */
+public final class ValueListenerTest {
+    @Mock
+    private SemanticErrorListener errorListener;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @After
+    public void postConditions() {
+        verifyNoMoreInteractions(errorListener);
+    }
+
+    @Test
+    public void test() throws IOException {
+        final CharStream stream = new ANTLRInputStream(SectionScopeListenerTest
+            .class
+            .getClassLoader()
+            .getResourceAsStream("SectionTest.spec"));
+        final SpeckyLexer lexer = new SpeckyLexer(stream);
+        final Specky parser = new Specky(new UnbufferedTokenStream<CommonToken>(lexer));
+
+        final SpecTypeResolver typeResolver =
+            new SpecTypeResolver();
+        final SectionScopeResolver sectionScopeResolver =
+            new SectionScopeResolver(errorListener, typeResolver);
+        final FileTypeListener fileTypeListener =
+            new FileTypeListener(typeResolver);
+        final SectionLicenceListener sectionLicenceListener =
+            new SectionLicenceListener(sectionScopeResolver);
+        final SectionImportTypeListener sectionImportTypeListener =
+            new SectionImportTypeListener(sectionScopeResolver);
+        final SectionImportValueListener sectionImportValueListener =
+            new SectionImportValueListener(sectionScopeResolver);
+        final SectionScopeListener sectionScopeListener =
+            new SectionScopeListener(sectionScopeResolver);
+        final SectionAuthorListener sectionAuthorListener =
+            new SectionAuthorListener(sectionScopeResolver);
+        final SectionPackageListener sectionPackageListener =
+            new SectionPackageListener(sectionScopeResolver);
+
+        parser.addParseListener(fileTypeListener);
+        parser.addParseListener(sectionLicenceListener);
+        parser.addParseListener(sectionImportTypeListener);
+        parser.addParseListener(sectionImportValueListener);
+        parser.addParseListener(sectionScopeListener);
+        parser.addParseListener(sectionAuthorListener);
+        parser.addParseListener(sectionPackageListener);
+
+        final Specky.SpecContext spec = parser.spec();
+
+        final AbstractTypeListener abstractTypeListener = new AbstractTypeListener(sectionScopeResolver);
+        ParseTreeWalker.DEFAULT.walk(abstractTypeListener, spec);
+        final Map<String, AbstractTypeDesc> abstractTypes = abstractTypeListener
+            .getAbstractTypeDescs()
+            .stream()
+            .collect(toMap(abstractTypeDesc -> abstractTypeDesc.getPackageName() + "." + abstractTypeDesc.getName(), abstractTypeDesc -> abstractTypeDesc));
+
+        final ValueListener valueListener = new ValueListener(sectionScopeResolver, abstractTypes, errorListener);
+        ParseTreeWalker.DEFAULT.walk(valueListener, spec);
+
+        final List<ValueDesc> valueDescs = valueListener.getValueDescs();
+
+        assertEquals(1, valueDescs.size());
+
+        final ValueDesc valueDesc = valueDescs.get(0);
+
+        assertEquals("FirstValue", valueDesc.getName());
+        assertEquals("Matt Champion", valueDesc.getAuthor());
+        assertEquals("Value type $L.\n\nAuto-generated from specification.", valueDesc.getDescription());
+        assertEquals("com.example", valueDesc.getPackageName());
+        assertEquals("default licence", valueDesc.getLicence());
+        assertEquals(singletonList("TestType"), valueDesc.getSupertypes());
+        assertEquals(ConstructionMethod.CONSTRUCTOR, valueDesc.getConstructionMethod());
+        final PropertyDesc p0 = PropertyDesc
+            .builder()
+            .name("num")
+            .type("Integer")
+            .typeParameters(emptyList())
+            .override(true)
+            .optional(false)
+            .defaultValue(CodeBlock.of("null"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        final PropertyDesc p1 = PropertyDesc
+            .builder()
+            .name("str")
+            .type("String")
+            .typeParameters(emptyList())
+            .override(false)
+            .optional(false)
+            .defaultValue(CodeBlock.of("null"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        assertThat(
+            valueDesc.getProperties(),
+            containsInAnyOrder(
+                p0,
+                p1));
+    }
+}
