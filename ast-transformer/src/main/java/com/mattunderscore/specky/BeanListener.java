2cc37bf4868ad75d7af043ba7685f31fb777a766
Matt Champion <mattunderscorechampion@gmail.com>
2017-01-12 21:47:29 +0000

    Add bean type implementation listener.

---
+++ ast-transformer/src/main/java/com/mattunderscore/specky/BeanListener.java
@@ -0,0 +1,387 @@
+/* Copyright © 2017 Matthew Champion All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.specky;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import com.mattunderscore.specky.constraint.model.NFConjoinedDisjointPredicates;
+import com.mattunderscore.specky.constraint.model.NFDisjointPredicates;
+import com.mattunderscore.specky.dsl.ConstraintFactory;
+import com.mattunderscore.specky.model.AbstractTypeDesc;
+import com.mattunderscore.specky.model.BeanDesc;
+import com.mattunderscore.specky.model.ConstructionMethod;
+import com.mattunderscore.specky.model.PropertyDesc;
+import com.mattunderscore.specky.model.generator.scope.Scope;
+import com.mattunderscore.specky.model.generator.scope.SectionScopeResolver;
+import com.mattunderscore.specky.parser.Specky;
+import com.mattunderscore.specky.parser.SpeckyBaseListener;
+import com.mattunderscore.specky.proposition.Normaliser;
+import com.squareup.javapoet.CodeBlock;
+
+import net.jcip.annotations.NotThreadSafe;
+
+/**
+ * Listener for value types.
+ * @author Matt Champion 05/01/2017
+ */
+@NotThreadSafe
+public final class BeanListener extends SpeckyBaseListener {
+    private final Normaliser normaliser = new Normaliser();
+    private final ConstraintFactory constraintFactory = new ConstraintFactory();
+    private final SectionScopeResolver sectionScopeResolver;
+    private final Map<String, AbstractTypeDesc> abstractTypes;
+    private final SemanticErrorListener semanticErrorListener;
+    private final List<BeanDesc> valueDescs = new ArrayList<>();
+
+    private Specky.ImplementationSpecContext implementationSpecContext;
+    private BeanDesc.Builder currentTypeDesc = BeanDesc.builder();
+    private String currentSection;
+    private List<String> currentSupertypes;
+
+    /**
+     * Constructor.
+     */
+    public BeanListener(
+            SectionScopeResolver sectionScopeResolver,
+            Map<String, AbstractTypeDesc> abstractTypes,
+            SemanticErrorListener semanticErrorListener) {
+
+        this.sectionScopeResolver = sectionScopeResolver;
+        this.abstractTypes = abstractTypes;
+        this.semanticErrorListener = semanticErrorListener;
+    }
+
+    /**
+     * @return the bean types
+     */
+    public List<BeanDesc> getBeanDescs() {
+        return unmodifiableList(valueDescs);
+    }
+
+    @Override
+    public void enterImplementationSpec(Specky.ImplementationSpecContext ctx) {
+        currentTypeDesc = BeanDesc.builder();
+        currentSupertypes = emptyList();
+        implementationSpecContext = ctx;
+    }
+
+    @Override
+    public void enterSupertypes(Specky.SupertypesContext ctx) {
+        final List<String> supertypes = ctx
+            .Identifier()
+            .stream()
+            .map(TerminalNode::getText)
+            .collect(toList());
+        currentSupertypes = supertypes;
+        currentTypeDesc = currentTypeDesc.supertypes(supertypes);
+    }
+
+    @Override
+    public void exitProps(Specky.PropsContext ctx) {
+        if (!isBean()) {
+            return;
+        }
+
+        final List<AbstractTypeDesc> resolveSupertypes =
+            resolveSupertypes(currentSupertypes, sectionScopeResolver.resolve(currentSection));
+
+        final List<PropertyDesc> inheritedProperties = resolveSupertypes
+            .stream()
+            .map(AbstractTypeDesc::getProperties)
+            .flatMap(Collection::stream)
+            .collect(toList());
+
+        final List<PropertyDesc> declaredProperties = ctx
+            .property()
+            .stream()
+            .map(this::createProperty)
+            .collect(toList());
+
+        final Map<String, PropertyDesc> knownProperties = new HashMap<>();
+        final List<PropertyDesc> allProperties = new ArrayList<>();
+        for (final PropertyDesc property : inheritedProperties) {
+            final String propertyName = property.getName();
+            final PropertyDesc currentProperty = knownProperties.get(propertyName);
+            if (currentProperty == null) {
+                allProperties.add(property);
+                knownProperties.put(propertyName, property);
+            }
+            else {
+                checkMergableProperties(currentProperty, property);
+            }
+        }
+        for (final PropertyDesc property : declaredProperties) {
+            final String propertyName = property.getName();
+            final PropertyDesc currentProperty = knownProperties.get(propertyName);
+            if (currentProperty == null) {
+                allProperties.add(property);
+                knownProperties.put(propertyName, property);
+            }
+            else {
+                final PropertyDesc mergedProperty = mergeDeclaredProperty(currentProperty, property);
+                allProperties.remove(currentProperty);
+                allProperties.add(mergedProperty);
+                knownProperties.put(propertyName, mergedProperty);
+            }
+        }
+
+        currentTypeDesc.properties(allProperties);
+    }
+
+    @Override
+    public void enterSectionDeclaration(Specky.SectionDeclarationContext ctx) {
+        currentSection = toValue(ctx.string_value());
+    }
+
+    @Override
+    public void enterOpts(Specky.OptsContext ctx) {
+        if (!isBean()) {
+            return;
+        }
+
+        currentTypeDesc = currentTypeDesc
+            .constructionMethod(toConstructionDesc(ctx))
+            .withModification(withModifications(ctx));
+    }
+
+    @Override
+    public void exitLicence(Specky.LicenceContext ctx) {
+        currentTypeDesc.licence(null);
+    }
+
+    @Override
+    public void exitImplementationSpec(Specky.ImplementationSpecContext ctx) {
+        if (!isBean()) {
+            currentTypeDesc = BeanDesc.builder();
+            return;
+        }
+
+        currentTypeDesc = currentTypeDesc
+            .name(ctx.Identifier().getText())
+            .author(sectionScopeResolver.resolve(currentSection).getAuthor())
+            .packageName(sectionScopeResolver.resolve(currentSection).getPackage())
+            .ifThen(
+                ctx.licence() == null,
+                builder -> builder
+                    .licence(sectionScopeResolver
+                        .resolve(currentSection)
+                        .getLicenceResolver()
+                        .resolve((String) null)
+                        .get()))
+            .ifThen(
+                ctx.licence() != null && ctx.licence().string_value() != null,
+                builder -> builder.licence(toValue(ctx.licence().string_value())))
+            .ifThen(
+                ctx.licence() != null && ctx.licence().Identifier() != null,
+                builder -> builder
+                    .licence(sectionScopeResolver
+                        .resolve(currentSection)
+                        .getLicenceResolver()
+                        .resolve(ctx.licence().Identifier().getText())
+                        .get()))
+            .ifThen(
+                ctx.StringLiteral() == null,
+                builder -> builder.description("Bean type $L.\n\nAuto-generated from specification."))
+            .ifThen(
+                ctx.StringLiteral() != null,
+                builder -> builder.description(
+                    ctx.StringLiteral().getText().substring(1, ctx.StringLiteral().getText().length() - 1)));
+
+        valueDescs.add(currentTypeDesc.build());
+    }
+
+    private List<AbstractTypeDesc> resolveSupertypes(List<String> supertypes, Scope scope) {
+        final List<AbstractTypeDesc> typeDescs = new ArrayList<>();
+        resolveSupertypes(supertypes, scope, typeDescs, new HashSet<>());
+        return typeDescs;
+    }
+
+    private void resolveSupertypes(
+        List<String> supertypes,
+        Scope scope,
+        List<AbstractTypeDesc> typeDescs,
+        Set<AbstractTypeDesc> setOfTypes) {
+
+        supertypes
+            .stream()
+            .map(typeName -> {
+                final Optional<String> optionalType = scope.getTypeResolver().resolve(typeName);
+                return optionalType.orElseGet(() -> {
+                    semanticErrorListener.onSemanticError("No resolvable type for " + typeName);
+                    return "unknown type";
+                });
+            })
+            .map(abstractTypes::get).forEach(typeDesc -> {
+            if (setOfTypes.add(typeDesc)) {
+                resolveSupertypes(typeDesc.getSupertypes(), scope, typeDescs, setOfTypes);
+                typeDescs.add(typeDesc);
+            }
+        });
+    }
+
+    private boolean isBean() {
+        return implementationSpecContext != null && implementationSpecContext.BEAN() != null;
+    }
+
+    private PropertyDesc createProperty(Specky.PropertyContext context) {
+        final String defaultValue = context.default_value() == null ?
+            null :
+            context.default_value().ANYTHING().getText();
+        final Specky.TypeParametersContext parametersContext = context
+            .typeParameters();
+        final List<String> typeParameters = parametersContext == null ?
+            emptyList() :
+            parametersContext
+                .Identifier()
+                .stream()
+                .map(ParseTree::getText)
+                .collect(toList());
+
+        final CodeBlock defaultCode = defaultValue != null ? CodeBlock.of(defaultValue) : sectionScopeResolver
+            .resolve(currentSection)
+            .getValueResolver()
+            .resolve(context.Identifier().getText(), context.OPTIONAL() != null).get();
+
+        return PropertyDesc
+            .builder()
+            .name(context
+                .propertyName()
+                .getText())
+            .type(context
+                .Identifier()
+                .getText())
+            .typeParameters(typeParameters)
+            .optional(context.OPTIONAL() != null)
+            .defaultValue(defaultCode)
+            .constraint(normaliser
+                .normalise(constraintFactory
+                    .create(context.propertyName().getText(), context.constraint_statement())))
+            .description(context.StringLiteral() == null ?
+                null :
+                context.StringLiteral().getText().substring(1, context.StringLiteral().getText().length() - 1))
+            .build();
+    }
+
+    private PropertyDesc mergeDeclaredProperty(PropertyDesc currentProperty, PropertyDesc declaredProperty) {
+        checkMergableProperties(currentProperty, declaredProperty);
+
+        final List<NFDisjointPredicates> constraints = new ArrayList<>();
+
+        if (currentProperty.getConstraint() != null) {
+            constraints.addAll(currentProperty.getConstraint().getPredicates());
+        }
+        if (declaredProperty.getConstraint() != null) {
+            constraints.addAll(declaredProperty.getConstraint().getPredicates());
+        }
+
+        return PropertyDesc
+            .builder()
+            .name(declaredProperty.getName())
+            .description(declaredProperty.getDescription())
+            .type(declaredProperty.getType())
+            .typeParameters(declaredProperty.getTypeParameters())
+            .optional(declaredProperty.isOptional())
+            .defaultValue(declaredProperty.getDefaultValue())
+            .override(true)
+            .constraint(NFConjoinedDisjointPredicates
+                .builder()
+                .predicates(constraints)
+                .build())
+            .build();
+    }
+
+    private void checkMergableProperties(PropertyDesc currentProperty, PropertyDesc newProperty) {
+        if (!newProperty.getType().equals(currentProperty.getType())) {
+            semanticErrorListener.onSemanticError("Conflicting property declarations for " +
+                currentProperty.getName() +
+                ". Types " +
+                currentProperty.getType() +
+                " and " +
+                newProperty.getType());
+        }
+        else if (newProperty.isOptional() != currentProperty.isOptional()) {
+            semanticErrorListener.onSemanticError("Conflicting property declarations for " +
+                currentProperty.getName() +
+                ". Cannot be both optional and required.");
+        }
+    }
+
+    private static String toValue(Specky.String_valueContext stringValue) {
+        if (stringValue == null) {
+            return null;
+        }
+
+        final TerminalNode multiline = stringValue.MULTILINE_STRING_LITERAL();
+        if (multiline != null) {
+            final String literal = multiline.getText();
+            return literal.substring(3, literal.length() - 3);
+        }
+        else {
+            final String literal = stringValue.StringLiteral().getText();
+            return literal.substring(1, literal.length() - 1);
+        }
+    }
+
+    private static ConstructionMethod toConstructionDesc(Specky.OptsContext options) {
+        if (options == null || options.construction() == null) {
+            return ConstructionMethod.CONSTRUCTOR;
+        }
+
+        final String token = options.construction().getText();
+        if ("constructor".equals(token)) {
+            return ConstructionMethod.CONSTRUCTOR;
+        }
+        else if ("builder".equals(token)) {
+            return ConstructionMethod.MUTABLE_BUILDER;
+        }
+        else if ("immutable builder".equals(token)) {
+            return ConstructionMethod.IMMUTABLE_BUILDER;
+        }
+        else if ("from defaults".equals(token)) {
+            return ConstructionMethod.FROM_DEFAULTS;
+        }
+        else {
+            throw new IllegalArgumentException("Unsupported type");
+        }
+    }
+
+    private static boolean withModifications(Specky.OptsContext options) {
+        return !(options == null || options.WITH_MODIFICATION() == null);
+    }
+}
---
+++ ast-transformer/src/test/java/com/mattunderscore/specky/BeanListenerTest.java
@@ -0,0 +1,145 @@
+package com.mattunderscore.specky;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toMap;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.UnbufferedTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.mattunderscore.specky.constraint.model.NFConjoinedDisjointPredicates;
+import com.mattunderscore.specky.model.AbstractTypeDesc;
+import com.mattunderscore.specky.model.BeanDesc;
+import com.mattunderscore.specky.model.ConstructionMethod;
+import com.mattunderscore.specky.model.PropertyDesc;
+import com.mattunderscore.specky.model.ValueDesc;
+import com.mattunderscore.specky.model.generator.scope.SectionScopeResolver;
+import com.mattunderscore.specky.parser.Specky;
+import com.mattunderscore.specky.parser.SpeckyLexer;
+import com.mattunderscore.specky.type.resolver.SpecTypeResolver;
+import com.squareup.javapoet.CodeBlock;
+
+/**
+ * Unit tests for {@link AbstractTypeListener}.
+ *
+ * @author Matt Champion 09/01/2017
+ */
+public final class BeanListenerTest {
+    @Mock
+    private SemanticErrorListener errorListener;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @After
+    public void postConditions() {
+        verifyNoMoreInteractions(errorListener);
+    }
+
+    @Test
+    public void test() throws IOException {
+        final CharStream stream = new ANTLRInputStream(SectionScopeListenerTest
+            .class
+            .getClassLoader()
+            .getResourceAsStream("SectionTest.spec"));
+        final SpeckyLexer lexer = new SpeckyLexer(stream);
+        final Specky parser = new Specky(new UnbufferedTokenStream<CommonToken>(lexer));
+
+        final SpecTypeResolver typeResolver =
+            new SpecTypeResolver();
+        final SectionScopeResolver sectionScopeResolver =
+            new SectionScopeResolver(errorListener, typeResolver);
+        final FileTypeListener fileTypeListener =
+            new FileTypeListener(typeResolver);
+        final SectionLicenceListener sectionLicenceListener =
+            new SectionLicenceListener(sectionScopeResolver);
+        final SectionImportTypeListener sectionImportTypeListener =
+            new SectionImportTypeListener(sectionScopeResolver);
+        final SectionImportValueListener sectionImportValueListener =
+            new SectionImportValueListener(sectionScopeResolver);
+        final SectionScopeListener sectionScopeListener =
+            new SectionScopeListener(sectionScopeResolver);
+        final SectionAuthorListener sectionAuthorListener =
+            new SectionAuthorListener(sectionScopeResolver);
+        final SectionPackageListener sectionPackageListener =
+            new SectionPackageListener(sectionScopeResolver);
+
+        parser.addParseListener(fileTypeListener);
+        parser.addParseListener(sectionLicenceListener);
+        parser.addParseListener(sectionImportTypeListener);
+        parser.addParseListener(sectionImportValueListener);
+        parser.addParseListener(sectionScopeListener);
+        parser.addParseListener(sectionAuthorListener);
+        parser.addParseListener(sectionPackageListener);
+
+        final Specky.SpecContext spec = parser.spec();
+
+        final AbstractTypeListener abstractTypeListener = new AbstractTypeListener(sectionScopeResolver);
+        ParseTreeWalker.DEFAULT.walk(abstractTypeListener, spec);
+        final Map<String, AbstractTypeDesc> abstractTypes = abstractTypeListener
+            .getAbstractTypeDescs()
+            .stream()
+            .collect(toMap(abstractTypeDesc -> abstractTypeDesc.getPackageName() + "." + abstractTypeDesc.getName(), abstractTypeDesc -> abstractTypeDesc));
+
+        final BeanListener beanListener = new BeanListener(sectionScopeResolver, abstractTypes, errorListener);
+        ParseTreeWalker.DEFAULT.walk(beanListener, spec);
+
+        final List<BeanDesc> beanDescs = beanListener.getBeanDescs();
+
+        assertEquals(1, beanDescs.size());
+
+        final BeanDesc beanDesc = beanDescs.get(0);
+
+        assertEquals("FirstBean", beanDesc.getName());
+        assertNull(beanDesc.getAuthor());
+        assertEquals("Bean type $L.\n\nAuto-generated from specification.", beanDesc.getDescription());
+        assertEquals("com.example", beanDesc.getPackageName());
+        assertEquals("default licence of named section", beanDesc.getLicence());
+        assertEquals(emptyList(), beanDesc.getSupertypes());
+        assertEquals(ConstructionMethod.CONSTRUCTOR, beanDesc.getConstructionMethod());
+        final PropertyDesc p0 = PropertyDesc
+            .builder()
+            .name("num")
+            .type("Integer")
+            .typeParameters(emptyList())
+            .override(false)
+            .optional(false)
+            .defaultValue(CodeBlock.of("5"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        final PropertyDesc p1 = PropertyDesc
+            .builder()
+            .name("str")
+            .type("String")
+            .typeParameters(emptyList())
+            .override(false)
+            .optional(false)
+            .defaultValue(CodeBlock.of("null"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        assertThat(
+            beanDesc.getProperties(),
+            containsInAnyOrder(
+                p0,
+                p1));
+    }
+}
