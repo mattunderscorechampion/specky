b25f9a5ddc12b7bd5a91af5aff96f2d1506a2c14
Matt Champion <mattunderscorechampion@gmail.com>
2017-01-14 23:59:27 +0000

    Add model generator that uses the listeners to parse an input stream and generate a model.

---
+++ ast-transformer/src/main/java/com/mattunderscore/specky/ModelGenerator.java
@@ -0,0 +1,129 @@
+/* Copyright © 2017 Matthew Champion All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.specky;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.UnbufferedTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import com.mattunderscore.specky.model.AbstractTypeDesc;
+import com.mattunderscore.specky.model.BeanDesc;
+import com.mattunderscore.specky.model.ImplementationDesc;
+import com.mattunderscore.specky.model.SpecDesc;
+import com.mattunderscore.specky.model.TypeDesc;
+import com.mattunderscore.specky.model.ValueDesc;
+import com.mattunderscore.specky.model.generator.scope.SectionScopeResolver;
+import com.mattunderscore.specky.parser.Specky;
+import com.mattunderscore.specky.parser.SpeckyLexer;
+import com.mattunderscore.specky.type.resolver.SpecTypeResolver;
+
+/**
+ * Processor for the ANTLR4 generated AST. Returns a better representation of the DSL.
+ *
+ * @author Matt Champion 14/01/2017
+ */
+public final class ModelGenerator {
+
+    /**
+     * @return the list of {@link SpecDesc} from a {@link CharStream}
+     */
+    public SpecDesc build(CharStream input, SemanticErrorListener errorListener) {
+        final SpeckyLexer lexer = new SpeckyLexer(input);
+        final Specky parser = new Specky(new UnbufferedTokenStream<CommonToken>(lexer));
+
+        final SpecTypeResolver typeResolver =
+            new SpecTypeResolver();
+        final SectionScopeResolver sectionScopeResolver =
+            new SectionScopeResolver(errorListener, typeResolver);
+        final FileTypeListener fileTypeListener =
+            new FileTypeListener(typeResolver);
+        final SectionLicenceListener sectionLicenceListener =
+            new SectionLicenceListener(sectionScopeResolver);
+        final SectionImportTypeListener sectionImportTypeListener =
+            new SectionImportTypeListener(sectionScopeResolver);
+        final SectionImportValueListener sectionImportValueListener =
+            new SectionImportValueListener(sectionScopeResolver);
+        final SectionScopeListener sectionScopeListener =
+            new SectionScopeListener(sectionScopeResolver);
+        final SectionAuthorListener sectionAuthorListener =
+            new SectionAuthorListener(sectionScopeResolver);
+        final SectionPackageListener sectionPackageListener =
+            new SectionPackageListener(sectionScopeResolver);
+
+        parser.addParseListener(fileTypeListener);
+        parser.addParseListener(sectionLicenceListener);
+        parser.addParseListener(sectionImportTypeListener);
+        parser.addParseListener(sectionImportValueListener);
+        parser.addParseListener(sectionScopeListener);
+        parser.addParseListener(sectionAuthorListener);
+        parser.addParseListener(sectionPackageListener);
+
+        final Specky.SpecContext spec = parser.spec();
+
+        final AbstractTypeListener abstractTypeListener = new AbstractTypeListener(sectionScopeResolver);
+        ParseTreeWalker.DEFAULT.walk(abstractTypeListener, spec);
+        final List<AbstractTypeDesc> abstractTypes = abstractTypeListener
+            .getAbstractTypeDescs();
+        final Map<String, AbstractTypeDesc> nameToAbstractType = abstractTypes
+            .stream()
+            .collect(toMap(abstractTypeDesc -> abstractTypeDesc.getPackageName() + "." + abstractTypeDesc.getName(), abstractTypeDesc -> abstractTypeDesc));
+
+        final ValueListener valueListener = new ValueListener(sectionScopeResolver, nameToAbstractType, errorListener);
+        ParseTreeWalker.DEFAULT.walk(valueListener, spec);
+
+        final BeanListener beanListener = new BeanListener(sectionScopeResolver, nameToAbstractType, errorListener);
+        ParseTreeWalker.DEFAULT.walk(beanListener, spec);
+
+        final List<BeanDesc> beanDescs = beanListener.getBeanDescs();
+        final List<ValueDesc> valueDescs = valueListener.getValueDescs();
+
+        final List<ImplementationDesc> implementations = Stream
+            .concat(
+                beanDescs.stream(),
+                valueDescs.stream())
+            .collect(toList());
+
+        final List<TypeDesc> types = Stream
+            .concat(
+                abstractTypes.stream(),
+                implementations.stream())
+            .collect(toList());
+
+        return SpecDesc
+            .builder()
+            .abstractTypes(abstractTypes)
+            .implementations(implementations)
+            .types(types)
+            .build();
+    }
+}
---
+++ ast-transformer/src/test/java/com/mattunderscore/specky/ModelGeneratorTest.java
@@ -0,0 +1,150 @@
+package com.mattunderscore.specky;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.mattunderscore.specky.constraint.model.NFConjoinedDisjointPredicates;
+import com.mattunderscore.specky.model.AbstractTypeDesc;
+import com.mattunderscore.specky.model.ConstructionMethod;
+import com.mattunderscore.specky.model.ImplementationDesc;
+import com.mattunderscore.specky.model.PropertyDesc;
+import com.mattunderscore.specky.model.SpecDesc;
+import com.mattunderscore.specky.model.TypeDesc;
+import com.squareup.javapoet.CodeBlock;
+
+/**
+ * Unit tests for {@link ModelGenerator}.
+ *
+ * @author Matt Champion 07/01/2017
+ */
+public final class ModelGeneratorTest {
+    @Mock
+    private SemanticErrorListener errorListener;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void build() throws Exception {
+        final CharStream stream = new ANTLRInputStream(SectionImportValueListenerTest
+            .class
+            .getClassLoader()
+            .getResourceAsStream("SectionTest.spec"));
+
+        final ModelGenerator modelGenerator = new ModelGenerator();
+
+        final SpecDesc specDesc = modelGenerator.build(stream, errorListener);
+
+        final List<TypeDesc> types = specDesc.getTypes();
+        final List<AbstractTypeDesc> abstractTypes = specDesc.getAbstractTypes();
+        final List<ImplementationDesc> implementations = specDesc.getImplementations();
+
+        assertEquals(3, types.size());
+        assertEquals(1, abstractTypes.size());
+        assertEquals(2, implementations.size());
+
+        final AbstractTypeDesc abstractType = abstractTypes.get(0);
+
+        assertEquals("TestType", abstractType.getName());
+        assertEquals("Matt Champion", abstractType.getAuthor());
+        assertEquals("Abstract type $L.\n\nAuto-generated from specification.", abstractType.getDescription());
+        assertEquals("com.example", abstractType.getPackageName());
+        assertEquals("default licence", abstractType.getLicence());
+        assertEquals(emptyList(), abstractType.getSupertypes());
+        assertThat(
+            abstractType.getProperties(),
+            contains(PropertyDesc
+                .builder()
+                .name("num")
+                .type("Integer")
+                .defaultValue(CodeBlock.of("null"))
+                .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+                .build()));
+
+        final ImplementationDesc valueDesc = implementations.get(1);
+
+        assertEquals("FirstValue", valueDesc.getName());
+        assertEquals("Matt Champion", valueDesc.getAuthor());
+        assertEquals("Value type $L.\n\nAuto-generated from specification.", valueDesc.getDescription());
+        assertEquals("com.example", valueDesc.getPackageName());
+        assertEquals("default licence", valueDesc.getLicence());
+        assertEquals(singletonList("TestType"), valueDesc.getSupertypes());
+        assertEquals(ConstructionMethod.CONSTRUCTOR, valueDesc.getConstructionMethod());
+        final PropertyDesc vp0 = PropertyDesc
+            .builder()
+            .name("num")
+            .type("Integer")
+            .typeParameters(emptyList())
+            .override(true)
+            .optional(false)
+            .defaultValue(CodeBlock.of("null"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        final PropertyDesc vp1 = PropertyDesc
+            .builder()
+            .name("str")
+            .type("String")
+            .typeParameters(emptyList())
+            .override(false)
+            .optional(false)
+            .defaultValue(CodeBlock.of("null"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        assertThat(
+            valueDesc.getProperties(),
+            containsInAnyOrder(
+                vp0,
+                vp1));
+
+        final ImplementationDesc beanDesc = implementations.get(0);
+
+        assertEquals("FirstBean", beanDesc.getName());
+        assertNull(beanDesc.getAuthor());
+        assertEquals("Bean type $L.\n\nAuto-generated from specification.", beanDesc.getDescription());
+        assertEquals("com.example", beanDesc.getPackageName());
+        assertEquals("default licence of named section", beanDesc.getLicence());
+        assertEquals(emptyList(), beanDesc.getSupertypes());
+        assertEquals(ConstructionMethod.CONSTRUCTOR, beanDesc.getConstructionMethod());
+        final PropertyDesc bp0 = PropertyDesc
+            .builder()
+            .name("num")
+            .type("Integer")
+            .typeParameters(emptyList())
+            .override(false)
+            .optional(false)
+            .defaultValue(CodeBlock.of("5"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        final PropertyDesc bp1 = PropertyDesc
+            .builder()
+            .name("str")
+            .type("String")
+            .typeParameters(emptyList())
+            .override(false)
+            .optional(false)
+            .defaultValue(CodeBlock.of("null"))
+            .constraint(NFConjoinedDisjointPredicates.builder().predicates(emptyList()).build())
+            .build();
+        assertThat(
+            beanDesc.getProperties(),
+            containsInAnyOrder(
+                bp0,
+                bp1));
+    }
+}
